6e10e81c8841002c60c969d873e1f9b6636b3840
paolapatino <pp020@hdm-stuttgart.de>
2018-04-23 13:11:32 +0200

    Zusätzliche Seiten von der Vorlesung

---
+++ Upload.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: paolapatino
+ * Date: 23.04.18
+ * Time: 12:46
+ */
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <title>CSS Beispiel Riemke</title>
+</head>
+<body>
+<form action="upload.php" method="post"
+      enctype="multipart/form-data">
+    Datei auswählen:
+    <input type="file" name="uploadfile"
+           id="uploadfile"><br>
+    <input type="submit" value="Datei
+   hochladen" name="submit">
+</form>
+</body>
+</html>
+
+
