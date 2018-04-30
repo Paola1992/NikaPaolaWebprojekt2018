137c9bd1f45167158f5c0298dca2608e3ae70608
Nika <ng046@hdm-stuttgart.de>
2018-04-30 12:08:57 +0200

    Datenbankverbindung und Registrierungsseite

--- registration.php
+++ registration.php
@@ -21,7 +21,7 @@
     <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
     <link rel="stylesheet" type="text/css" href="">
 
-    <title>Registrierung</title>
+    <title>Registrierung </title>
 </head>
 <body>
 
