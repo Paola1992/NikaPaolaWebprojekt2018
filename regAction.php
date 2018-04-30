6178d1e8b3048a1f83da13c46a7b32659d39ce04
Nika <ng046@hdm-stuttgart.de>
2018-04-30 12:41:02 +0200

    Registrierung

--- regAction.php
+++ regAction.php
@@ -19,62 +19,61 @@ $passwordTwo = $_POST["passwordTwo"];
 
 //Prüfung ob Passwörter identisch oder leer oder Benutzername leer.
   if ($passwordOne != $passwordTwo OR $username == "" OR $passwordOne == "") {
-      header("Location:register_fehler.php");
+     /* header("Location:register_fehler.php");*/
       exit;
   }
 
   else {
 
-/*if (strlen($eingegebenesPasswort) <=5)
-{
-    header("Location:register_fehler_pwZuKurz.php");
-}
-else
-{
-*/
+      /*if (strlen($eingegebenesPasswort) <=5)
+      {
+          header("Location:register_fehler_pwZuKurz.php");
+      }
+      else
+      {
+      */
 
 //Funktion für das Verschlüsseln des eingegebenen Passworts
-    function hashPassword($passwordOne, $username)
-    {
-        return hash('sha256', $passwordOne . $username);
-    }
+      function hashPassword($passwordOne, $username)
+      {
+          return hash('sha256', $passwordOne . $username);
+      }
 
 //Verschlüsseln des Passworts
-    $hashedPassword = hashPassword($passwordOne, $username);
-
-
+      $hashedPassword = hashPassword($passwordOne, $username);
 
 
 //Prüfung ob Benutzer bereits in der Datanbank existiert
-    $counter = 0;
-    $sql = $dbConnect->prepare("SELECT username FROM user WHERE username = :username");
-    $sql->execute(array(':username' => $username));
-    while ($rows = $sql->fetchAll(PDO::FETCH_ASSOC)) {
-        $counter++;
-    }
+      $counter = 0;
+      $sql = $dbConnect->prepare("SELECT username FROM user WHERE username = :username");
+      $sql->execute(array(':username' => $username));
+      while ($rows = $sql->fetchAll(PDO::FETCH_ASSOC)) {
+          $counter++;
+      }
 
 //Wenn Benutzer nicht vorhanden, dann wird dieser eingetragen
-    if ($counter == 0) {
-        $insertUser = "INSERT INTO user (username, email, password) VALUES ('$username', '$email', '$hashedPassword')";
-        $sql = $dbConnect->prepare($insertUser);
-        $sql = $dbConnect->query($insertUser);
+      if ($counter == 0) {
+          $insertUser = "INSERT INTO user (username, email, password) VALUES ('$username', '$email', '$hashedPassword')";
+          $sql = $dbConnect->prepare($insertUser);
+          $sql = $dbConnect->query($insertUser);
+      }
 
 
-//Wenn erfolgreich wird der Benutzer eingeloggt
-        if ($insertUser == true) {
+      /* //Wenn erfolgreich wird der Benutzer eingeloggt
+              if ($insertUser == true) {
 
-            header('Location:welcome2.html');
-            exit;
-        } else {
-            echo "Fehler beim Speichern des Benutzernames. <a href=\"registration.php\">Zurück</a>";
-        }
-    } else {
-        //header("Location:register_fehler_benutzername.php");
-    }
+                  header('Location:welcome2.html');
+                  exit;
+              } else {
+                  echo "Fehler beim Speichern des Benutzernames. <a href=\"registration.php\">Zurück</a>";
+              }
+          } else {
+              //header("Location:register_fehler_benutzername.php");
+          }
 
-
-}
-
+      */
 
 
-?>
+  }
+
+  ?>
