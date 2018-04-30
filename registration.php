be8c6482b144a0f776dfac3a83b75d414aca13e0
paolapatino <pp020@hdm-stuttgart.de>
2018-04-30 20:56:56 +0200

    Registration.php mit Css bearbeitet

--- registration.php
+++ registration.php
@@ -16,46 +16,47 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Bootstrap -->
     <link href="css/styles.css" rel="stylesheet" type="text/css">
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-    <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" type="text/css" href="">
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+
 
     <title>Registrierung</title>
 </head>
 <body>
-
+<div class="site-wrapper">
 <!-- Registrieungsformular -->
  <main>
+     <div class="inner cover">
+        <h1 class="margin-base-vertical">Jetzt registrieren</h1>
+        <div class="container-fluid">
+            <div class="row">
 
-     <div class="container">
-         <div class="row">
 
+                 <div class="col-sm-6">
+                   <form enctype="multipart/form-data" action="regAction.php" method="post">
 
-             <div class="col-sm-6">
-                 <form enctype="multipart/form-data" action="regAction.php" method="post">
-                     <input type="text" name="username">
-                     <br>
-                     <input type="text" name="email">
-                     <br>
-                     <input type="text" name="passwordOne">
-                     <br>
-                     <input type="text" name="passwordTwo">
-                     <br>
-                     <input type="submit" name="submit">
-                 </form>
+                      <div class="form-group">
+                           <label for="user">Benutzername</label>
+                          <input type="text" class="form-control" id="user" placeholder="Username">
+                         </div>
+                         <div class="form-group">
+                             <label for="exampleInputEmail1">E-mail Adresse</label>
+                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                         </div>
+                        <div class="form-group">
+                            <label for="exampleInputPassword1">Passwort</label>
+                             <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                         </div><div class="form-group">
+                          <label for="exampleInputPassword1">Passwort wiederholen</label>
+                            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                         </div>
+                         <button type="submit" class="btn btn-primary">Senden</button>
+                     </form>
 
+                 </div>
              </div>
-         </div>
-
-
      </div>
-
  </main>
-<div>
-
-            </div>
-
-
+</div>
 </body>
 </html>
 
