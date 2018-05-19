98fc80791a408d0d687a10e4aea925a04034fe1a
Nika <ng046@hdm-stuttgart.de>
2018-05-19 11:56:07 +0200

    Alle Änderungen Registrierung vorgenommen

--- Upload verarbeiten I.php
+++ Upload verarbeiten I.php
@@ -9,15 +9,15 @@
 ?>
 
 <?php
-echo "Dateiname: ".$_FILES["uploadfile"]["name"]."<br>"; if($_FILES["uploadfile"]["name"]=="")
-{
+echo "Dateiname: ".$_FILES["uploadfile"]["name"]."<br>";
+
+if($_FILES["uploadfile"]["name"]=="") {
     echo "Fehler Dateiname.";
     die(); }
+
 $fileName=$_FILES["uploadfile"]["name"];
 $fileType=substr($fileName,strlen($fileName)-3,strlen($fileName) ); $fileName=substr($fileName,0,strlen($fileName)-4 );
 echo "FILENAME:".$fileName."FILETYPE:".$fileType."<br>";
-if ($_FILES["uploadfile"]["size"] > 800000) {
-    echo"Datei zu groß.";
-    die();
-}
+?>
+
 
