457a7a6a126fc845f79f1c1247705d772b45d7a3
Nika <ng046@hdm-stuttgart.de>
2018-05-21 19:28:27 +0200

    Fehler in deleteFile.php behoben

--- deleteFile.php
+++ deleteFile.php
@@ -12,7 +12,7 @@ $file2 = 'uploads/'.$file;
 
 if (file_exists($file2)) {
     unlink($file2);
-    $loeschen = $dbConnect->exec("DELETE FROM upload WHERE filename = '$file'");
+    $delete = $dbConnect->exec("DELETE FROM upload WHERE filename = '$file'");
     $query = $dbConnect->query($delete);
     header("location:homeDashboard.php");
 }
