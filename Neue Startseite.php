88928e879a5e1ab4e69bb7a0bf53292bdf268d56
Nika <ng046@hdm-stuttgart.de>
2018-06-18 12:05:24 +0200

    Share Option entwicklung

---
+++ Neue Startseite.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nikagruenen
+ * Date: 10.06.18
+ * Time: 17:42
+ */
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+
+    <title>Neue Startseite</title>
+</head>
+
+<body>
+<section class="intro"
+        <div class="inner"
+              <div class="content"
+              <h1>Safe and Send</h1> <br>
+                    <a class="btn" href="login.php" >Einloggen</a>
+                    <a class="btn" href="registration.php" >Registrieren</a>
+</div>
+</div>
+</section>
+
+</body>
+</html>
+
---
+++ share.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: paolapatino
+ * Date: 04.05.18
+ * Time: 12:24
+ */
+?>
+
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap -->
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+
+
+    <title>Safe&Send</title>
+
+</head>
+<body>
+<?php
+include "navigation.php";
+
+?>
+
+
+
+<div>
+
+    <form action="shareAction.php" method="post">Teilen<br>
+        <input type="text" name="username" style="color:black"/> <br>
+        Benutzername<br>
+        <input class="btn btn-primary btn-lg" href="shareAction.php" role="button" type="submit" value="freigeben"/>
+    </form>
+    <?php
+    include "shareFile.php";
+    ?>
+
+</div>
+
+
+
+
+</body>
+</html>
+
